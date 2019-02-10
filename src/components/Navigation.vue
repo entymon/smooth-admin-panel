2e9d6bcf752e4aa177af71d87baac58b66e58f8f
entymon <pawel.olejniczak.i@gmail.com>
2019-02-10 22:42:38 +0000

    Moved create new post button to list of post and avoid swithin edit and create without refresh data for same componentn

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -16,12 +16,6 @@
 				<b-navbar-nav class="ml-auto">
 
 					<b-nav-form>
-						<b-button v-on:click="createPost()" size="sm" variant="primary" class="mr-2 my-sm-0">
-							Add New
-						</b-button>
-					</b-nav-form>
-
-					<b-nav-form>
 						<b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
 						<b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
 					</b-nav-form>
@@ -50,9 +44,6 @@
 			postCreate: true,
 		}),
 		methods: {
-      createPost() {
-        this.$router.push({ name: 'post' })
-			},
 			userLogout() {
         LoginService.logout();
         this.$router.push({ name: 'dashboard' })
--- src/pages/dashboard/PostList.vue
+++ src/pages/dashboard/PostList.vue
@@ -1,6 +1,15 @@
 <template>
 	<div class="container-fluid">
-		<h3 class="header">List of posts</h3>
+		<div class="row my-3">
+			<div class="col">
+				<h3>List of posts</h3>
+			</div>
+			<div class="col text-right">
+				<b-button v-on:click="createPost()" size="sm" variant="primary" class="mr-2 my-sm-0">
+					Add new post
+				</b-button>
+			</div>
+		</div>
 		<list-row :key="post.uuid" v-for="post in posts" :post="post" @editPost="updatePost"/>
 	</div>
 </template>
@@ -19,6 +28,9 @@
 			}
 		},
 		methods: {
+      createPost() {
+        this.$router.push({ name: 'post' })
+      },
       updatePost(postUuid) {
 				this.$router.push({ name: 'post', params: { uuid: postUuid } })
 			}
