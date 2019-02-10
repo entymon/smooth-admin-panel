cc7ca3712e258d0013c32890ce28df6a5274d730
entymon <pawel.olejniczak.i@gmail.com>
2019-02-10 23:14:35 +0000

    Valid list

--- src/components/blog/PostListRow.vue
+++ src/components/blog/PostListRow.vue
@@ -2,8 +2,8 @@
 	<div class="row my-2">
 		<div class="col-md-1">{{ post.uuid }}</div>
 		<div class="col-md-2">{{ post.title }}</div>
-		<div class="col-md-5">{{ post.description }}</div>
-		<div class="col-md-2">{{ post.imageUrl }}</div>
+		<div class="col-md-5">{{ post.body }}</div>
+		<div class="col-md-2">{{ post.image }}</div>
 		<div class="col-md-2">
 			<b-button @click=editRow class="mr-md-2">edit</b-button>
 			<b-button variant="danger">remove</b-button>
